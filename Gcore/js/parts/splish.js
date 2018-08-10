c2bc8ea0228cd92e77f928a31dfb3a4267ff1536
Chris Pro <prokic720x@gmail.com>
2018-08-10 16:29:30 +0200

    Add files via upload

---
+++ Gcore/js/parts/splish.js
@@ -0,0 +1,8 @@
+window.onscroll = function() {splish()};
+
+function splish() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("gl-bar").style.width = scrolled + "%";
+}
