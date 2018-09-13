3243269b804d0f8f5594f73746aff3c2e99d70b3
Chris Pro <prokic720x@gmail.com>
2018-09-13 05:00:58 +0200

    Update splish.js

--- Gcore/js/parts/splish.js
+++ Gcore/js/parts/splish.js
@@ -1,8 +1,12 @@
-window.onscroll = function() {splish();};
+"use strict";
+
+window.onscroll = function () {
+  splish();
+};
 
 function splish() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
+  var scrolled = winScroll / height * 100;
   document.getElementById("gl-bar").style.width = scrolled + "%";
 }
